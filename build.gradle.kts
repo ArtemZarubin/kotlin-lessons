17f834145155b26d8f1fe271176f4fa7d235e408
Artem Zarubin <zarubinartem652@gmail.com>
2023-07-01 00:20:06 +0400

    Update build system to Gradle and update .gitignore
    This commit migrates the project to Gradle as the build system, providing improved build configuration and dependency management. Additionally, the .gitignore file has been updated to exclude unnecessary files and directories from version control. These changes ensure a more efficient and organized development workflow.

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.8.21"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    // https://mvnrepository.com/artifact/com.google.code.gson/gson
+    implementation("com.google.code.gson:gson:2.10.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
+}
